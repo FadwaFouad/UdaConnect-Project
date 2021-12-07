5fad09f0202caf684e87fb602427cb038aad4604
FadwaFouad <fadoalqasem@gmail.com>
2021-12-07 08:46:52 +0200

    add logic of consumer

--- modules/create-location-grpc/client.py
+++ modules/create-location-grpc/client.py
@@ -5,38 +5,31 @@ from google.protobuf.json_format import MessageToJson
 from google.protobuf.timestamp_pb2 import Timestamp
 from kafka import KafkaProducer
 
-import grpc
 import location_pb2
-import location_pb2_grpc
 import time
 
 def run():
     logging.getLogger("run")
-    with grpc.insecure_channel('location-grpc:50051') as channel:
-        stub = location_pb2_grpc.LocationServiceStub(channel)
-        print("-------------- Client --------------")
+    print("-------------- Client --------------")
 
-        now = time.time()
-        seconds = int(now)
-        nanos = int((now - seconds) * 10**9)
-        timestamp = Timestamp(seconds=seconds, nanos=nanos)
+    now = time.time()
+    seconds = int(now)
+    nanos = int((now - seconds) * 10**9)
+    timestamp = Timestamp(seconds=seconds, nanos=nanos)
 
-        location = location_pb2.LocationMessage(
-            person_id = 1,
-            latitude =  99.0 ,
-            longitude =  44.5, 
-            creation_time = timestamp
-        )
+    location = location_pb2.LocationMessage(
+        person_id = 1,
+        latitude =  99.0 ,
+        longitude =  44.5, 
+        creation_time = timestamp
+    )
 
-        #send payload to kafka producer
-        KAFKA_SERVER = 'kafka-service:9092'
-        producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
-        kafka_data = MessageToJson(location).encode()
-        producer.send("locations", kafka_data)
-        producer.flush()
-
-        response = stub.Create(location)
-        logging.info("responces: "+ str(response))
+    #send payload to kafka producer
+    KAFKA_SERVER = 'kafka-service:9092'
+    producer = KafkaProducer(bootstrap_servers=KAFKA_SERVER)
+    kafka_data = MessageToJson(location).encode()
+    producer.send("locations", kafka_data)
+    producer.flush()
       
 
 if __name__ == '__main__':
--- modules/create-location-grpc/server.py
+++ modules/create-location-grpc/server.py
@@ -17,87 +17,33 @@ from google.protobuf.timestamp_pb2 import Timestamp
 
 
 class LocationServicer(location_pb2_grpc.LocationServiceServicer):
-    def Create(self, request, context):
+    def Create(self, location, context):
         logging.info("create-location-grpc")
 
-        # setup for consumer
-        consumer = KafkaConsumer(
-        'locations',
-        bootstrap_servers=['kafka-service:9092'],
-        auto_offset_reset='earliest',
-        enable_auto_commit=True,
-        group_id='my-group',
-        # request_timeout_ms=30000,
-        # max_poll_records=1
-        )
-        logging.info(f"consumer")
+        # use urllib3 to send new location to location service to save it into database
+        http = urllib3.PoolManager()
+        url = 'http://location-api:5000/api/locations'
 
+        # extract dateTime from timestamp 
+        timestamp : Timestamp = location.creation_time
+        creation_time = datetime.fromtimestamp(timestamp.seconds + timestamp.nanos/1e9)
 
-        # get data from consumer
-        location = {}
-        for message in consumer:
-            logging.info(f"message before  : {message}")
-            message = message.value.decode('utf-8')
-            logging.info(f"message after  : {message}")
-            location=location_pb2.LocationMessage()
-            Parse(str(message), location)
-            #locations.append(location)
-            logging.info(f"locations  : {location}")
+        request_payload = {
+            "person_id": location.person_id,
+            "longitude": str(location.longitude),
+            "latitude": str(location.longitude),
+            "creation_time": str(creation_time)
+            }
 
-            logging.info(f"after  : loop")
-            # use urllib3 to send new location to location service to save it into database
-            http = urllib3.PoolManager()
-            url = 'http://location-api:5000/api/locations'
-            # for i in range(len(locations)):
+        logging.info(f"request {request_payload}")
 
-            # extract dateTime from timestamp 
-            timestamp : Timestamp = location.creation_time
-            creation_time = datetime.fromtimestamp(timestamp.seconds + timestamp.nanos/1e9)
-
-
-            
-            logging.info(location.person_id)
-            # logging.info(location['person_id'])
-
-            request_payload = {
-                "person_id": location.person_id,
-                "longitude": str(location.longitude),
-                "latitude": str(location.longitude),
-                "creation_time": str(creation_time)
-                }
-
-            logging.info(f"request {request_payload}")
-            logging.info(f"request dumbs  {json.dumps(request_payload)}")
-
-            req = http.request('POST', url, body=json.dumps(request_payload), headers={'Content-Type': 'application/json'})
-            logging.info(f"request  : {req.data}")
-        logging.info(f" {location} Messages founded ")
-
-
-            
-            
-
-        # request_payload = {
-        #     "person_id": int(locations.index(i).person_id),
-        #     "longitude":str(locations.index(i).longitude),
-        #     "latitude": str(locations.index(i).latitude),
-        #     "creation_time": str(creation_time)
-        # }
-
-
-
-            # # create request data to post in Location Service 
-            # request_payload = {
-            # "person_id": int(locations[i]['person_id']),
-            # "longitude": str(locations[i]['longitude']),
-            # "latitude": str(locations[i]['latitude']),
-            # "creation_time": str(creation_time)
-            # }
+        req = http.request('POST', url, body=json.dumps(request_payload), headers={'Content-Type': 'application/json'})
+        logging.info(f"request  : {req.data}")
 
         responce = {
-            "person_id": int(request.person_id),
-            "longitude": request.longitude,
-            "latitude": request.latitude,
+            "person_id": int(location.person_id),
+            "longitude": location.longitude,
+            "latitude": location.latitude,
             "creation_time": timestamp
         }
         return location_pb2.LocationMessage(**responce)
